5eaf4dfb68dab933cb2a8b11094c51a8a35e8246
Андрей Тихобаев <atikhobaev@gmail.com>
2023-02-15 01:38:35 +0700

    first review fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -97,7 +97,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'api/static/'),
+    os.path.join(BASE_DIR, 'api/static_extra/'),
 )
 
 MEDIA_URL = '/media/'
